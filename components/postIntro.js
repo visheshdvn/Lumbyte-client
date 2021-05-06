2ea4d709ed433532955f85a67575b03fa0a90932
visheshdvn <vishesh.dh1@gmail.com>
2021-05-06 19:27:01 +0530

    index page responsive fixed

--- components/postIntro.js
+++ components/postIntro.js
@@ -1,69 +1,87 @@
 import React from "react"
+import Image from "next/image"
+import {
+  UserCircleIcon,
+  CalendarIcon,
+  ClockIcon,
+} from "@heroicons/react/outline"
 
 const postIntro = () => {
   return (
-    <section className="text-gray-600 body-font mb-20">
-      <div className="container mx-auto flex flex-col">
-        <h1 className="lg:text-4xl lg:mb-3 leading-6">Title of blog here</h1>
-        <div className="lg:w-full mx-auto">
-          <div className="h-64 overflow-hidden">
-            <img
-              alt="content"
-              className="object-cover object-center h-full w-full"
-              src="https://dummyimage.com/1200x500"
-            />
+    <article className="text-gray-600 body-font overflow-hidden md:mb-20 mb-10">
+      <div className="mx-auto flex md:flex-col">
+        <div className="w-auto md:order-1 order-2 md:max-w-max pl-2 md:pl-0">
+          <h1 className="text-base leading-5 lg:text-3xl md:text-2xl md:mb-3 mb-2 font-bold">
+            Elit Lorem eu voluptate consectetur duis laboris veniam labore
+            commodo fugiat amet enim ex ipsum.
+          </h1>
+          <div className="md:hidden font-normal">
+              <span>Date</span> {' | '}
+              <span>3Min read</span>
           </div>
-          <div className="flex flex-col sm:flex-row mt-5">
-            <div className="sm:w-1/3 text-center sm:pr-8 sm:py-0">
-              <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-10 h-10"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                  <circle cx="12" cy="7" r="4"></circle>
-                </svg>
-              </div>
-              <div className="flex flex-col items-center text-center justify-center">
-                <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">
+        </div>
+        {/* <div className="lg:w-full mx-auto"> */}
+        <div className="mobileImageSize overflow-hidden relative h-28 md:h-64 lg:h-72 md:w-full md:order-2 order-1">
+          <Image
+            alt="Post Banner"
+            // width={1024}
+            // height={350}
+            className="object-cover object-center"
+            src="https://source.unsplash.com/random/"
+            layout="fill"
+            objectFit="cover"
+          />
+          {/* </div> */}
+        </div>
+        <div className="md:flex flex-col sm:flex-row mt-5 md:order-3 order-3 hidden">
+          <div className="w-full md:w-1/4 text-center sm:pr-4 sm:py-0">
+            <div className="flex flex-col">
+              <div className="flex">
+                <UserCircleIcon className="h-5 w-5" />
+                <h2 className="pl-2 font-medium text-gray-900 text-sm text-left flex items-center mb-1">
                   Phoebe Caulfield
                 </h2>
-                <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+              </div>
+              <div className="flex">
+                <CalendarIcon className="h-5 w-5" />
+                <h2 className="pl-2 font-medium text-gray-900 text-sm text-left flex items-center mb-1">
+                  Date
+                </h2>
+              </div>
+              <div className="flex">
+                <ClockIcon className="h-5 w-5" />
+                <h2 className="pl-2 font-medium text-gray-900 text-sm text-left flex items-center">
+                  Minute read
+                </h2>
               </div>
             </div>
-            <div className="sm:w-2/3 sm:py-0 border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-              <p className="text-base mb-2">
-                Ea eu occaecat reprehenderit aliquip ad ullamco consequat irure
-                aliqua irure mollit culpa dolor. Nulla et veniam irure tempor
-                eiusmod quis anim voluptate nostrud. Fugiat minim et
-                reprehenderit consequat eu veniam ea enim. Minim amet irure
-                ipsum enim qui irure sint. Nisi mollit veniam id consequat
-                veniam.
-              </p>
-              <a className="text-indigo-500 inline-flex items-center cursor-pointer hover:bg-indigo-500 hover:text-white">
-                Read More
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 ml-2"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
+          </div>
+          <div className="hidden md:block w-3/4 py-0 border-gray-200 border-t-0 pt-0 mt-0 text-left">
+            <p className="text-base mb-2">
+              Ea eu occaecat reprehenderit aliquip ad ullamco consequat irure
+              aliqua irure mollit culpa dolor. Nulla et veniam irure tempor
+              eiusmod quis anim voluptate nostrud. Fugiat minim et reprehenderit
+              consequat eu veniam ea enim. Minim amet irure ipsum enim qui irure
+              sint. Nisi mollit veniam id consequat veniam.
+            </p>
+            <a className="text-indigo-500 inline-flex items-center cursor-pointer hover:bg-indigo-500 hover:text-white">
+              Read More
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </a>
           </div>
         </div>
       </div>
-    </section>
+    </article>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,10 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      borderWidth: ['last'],
+      margin: ['last', 'odd', 'even']
+    },
   },
   plugins: []
 }
