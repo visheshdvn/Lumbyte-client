f457a989065d71b2419c8be569f7729ff5c36941
visheshdvn <vishesh.dh1@gmail.com>
2021-06-05 01:22:34 +0530

    blog content headings custom omponents created

---
+++ components/blogtext/headings.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export const H1 = ({ children }) => {
+  return <h1 className="text-4xl font-pt-sans font-bold">{children}</h1>
+}
+
+export const H2 = ({ children }) => {
+  return <h2 className="text-3xl font-pt-sans font-bold">{children}</h2>
+}
+
+export const H3 = ({ children }) => {
+  return <h3 className="text-2xl font-pt-sans font-bold">{children}</h3>
+}
