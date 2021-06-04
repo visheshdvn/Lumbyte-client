0ea3f946b81ae57d5e47aba3b12c63bd9efe9e82
visheshdvn <vishesh.dh1@gmail.com>
2021-06-05 00:55:02 +0530

    blog content section in progress

---
+++ components/blogtext/paragraph.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Paragraph = ({children}) => {
+
+    return (
+        <p className="text-lg mb-7 leading-7.5 text-gray-700">
+            {children}
+        </p>
+    )
+}
+
+export default Paragraph
