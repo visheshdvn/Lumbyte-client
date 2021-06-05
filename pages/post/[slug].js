72e4b53aed3947f5f96205f98e9201b76b0b6482
visheshdvn <vishesh.dh1@gmail.com>
2021-06-05 13:08:57 +0530

    post page similar section made and quality enhancements

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -37,9 +37,9 @@ const Post = ({
       <Head>
         <meta name="description" content={metadescription} />
       </Head>
-      <div className="container mt-2 mx-auto horizontal-spacing pt-16">
-        <div className="grid gap-4 grid-cols-12">
-          <div className="col-span-8 ">
+      <section className="">
+        <div style={{borderColor: "rgba(0,0,0,0.20)"}} className="container mt-2 mx-auto horizontal-spacing pt-16 border-b">
+          <>
             <Link href={`/topic/${topic}`}>
               <a>
                 <h4 className="font-pt-sans uppercase font-bold text-lightBlue-600 text-lg pl-8 mb-3">
@@ -52,45 +52,95 @@ const Post = ({
               {title}
             </h1>
 
-            <div className="text-gray-400 px-8 mb-5">
-              <div className="flex items-center h-4 text-grayText overflow-hidden">
-                <CalendarIcon className="h-full inline mr-1" />
-                <span className="text-base font-coda">{stringdate}</span>
-                <span className="mx-2">|</span>
-                <ClockIcon className="h-full inline mr-1" />
-                <span className="text-base font-coda">{minutes} Min read</span>
-              </div>
+            <div className="flex items-center h-4 px-8 mb-5 text-grayText overflow-hidden">
+              <CalendarIcon className="h-full inline mr-1" />
+              <span className="text-base font-coda">{stringdate}</span>
+              <span className="mx-2">|</span>
+              <ClockIcon className="h-full inline mr-1" />
+              <span className="text-base font-coda">{minutes} Min read</span>
             </div>
+          </>
 
-            {/* <div className="h-96 overflow-hidden"> */}
-            <Image
-              src={`http://localhost:1337${banner.url}`}
-              width={1366}
-              height={950}
-              layout="responsive"
-              className="object-cover object-center"
-            />
-            <main key={id} className="px-8 mt-5">
-              <div className="prose lg:prose-lg font-serif">
-                <MDXRemote {...content} components={components} />
+          <div className="grid gap-4 grid-cols-12">
+            <div className="col-span-8 ">
+              {/* <div className="h-96 overflow-hidden"> */}
+              <Image
+                src={`http://localhost:1337${banner.url}`}
+                width={1366}
+                height={950}
+                layout="responsive"
+                className="object-cover object-center"
+              />
+              <main key={id} className="px-8 mt-5">
+                <div className="prose lg:prose-lg font-serif">
+                  <MDXRemote {...content} components={components} />
+                </div>
+              </main>
+              {/* </div> */}
+            </div>
+            <div className="col-span-4">
+              <div className="border-l pl-3 pb-2">
+                <h1 className="text-4xl font-pt-sans font-bold mb-4">
+                  Featured
+                </h1>
+                <h3 className="font-coda text-lg leading-6 mb-4">
+                  Ad adipisicing reprehenderit esse nostrud sint eu qui.
+                </h3>
+                <h3 className="font-coda text-lg leading-6 mb-4">
+                  Culpa Lorem esse nulla enim deserunt est voluptate occaecat
+                  exercitation aliquip do in pariatur veniam.
+                </h3>
+                <h3 className="font-coda text-lg leading-6 mb-4">
+                  Culpa Lorem esse nulla enim deserunt est veniam.
+                </h3>
+                <h3 className="font-coda text-lg leading-6 mb-4">
+                  Culpa Lorem esse nulla enim deserunt est voluptate occaecat.
+                </h3>
               </div>
-            </main>
-            {/* </div> */}
-          </div>
-          <div className="col-span-4 pt-32">
-            <div className="border-l pl-3 pb-2">
-              <h1 className="text-4xl font-pt-sans font-bold mb-4">
-                Featured
-              </h1>
-              <h3 className="font-coda text-lg leading-6 mb-4">Ad adipisicing reprehenderit esse nostrud sint eu qui.</h3>
-              <h3 className="font-coda text-lg leading-6 mb-4">Culpa Lorem esse nulla enim deserunt est voluptate occaecat exercitation aliquip do in pariatur veniam.</h3>
-              <h3 className="font-coda text-lg leading-6 mb-4">Culpa Lorem esse nulla enim deserunt est veniam.</h3>
-              <h3 className="font-coda text-lg leading-6 mb-4">Culpa Lorem esse nulla enim deserunt est voluptate occaecat.</h3>
             </div>
           </div>
         </div>
-      </div>
+      </section>
+      <section className="bg-gray-800 py-3 my-2">
+        <div className="container mx-auto">
+          <h1 className="font-pt-sans font-bold text-white text-4xl mb-2">
+            Similar
+          </h1>
+          <div className="flex justify-between">
+            <SimilarArticles />
+            <SimilarArticles />
+            <SimilarArticles />
+          </div>
+        </div>
+      </section>
     </>
+  )
+}
+
+function SimilarArticles() {
+  return (
+    <Link href="/">
+      <a className="px-2">
+        <div className="relative h-80 w-full overflow-hidden">
+          <Image
+            className="object-cover object-center"
+            src="https://source.unsplash.com/random"
+            alt="image"
+            width={1000}
+            height={1000}
+          />
+          <div
+            style={{
+              backgroundImage:
+                "linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.7) 80%, rgba(0,0,0,1) 99%)",
+            }}
+            className="absolute top-0 right-0 left-0 bottom-0"
+          />
+
+          <h3 className="absolute bottom-2 right-2 left-2 font-coda text-white text-lg leading-6">Voluptate adipisicing occaecat quis mollit et laborum commodo.</h3>
+        </div>
+      </a>
+    </Link>
   )
 }
 
