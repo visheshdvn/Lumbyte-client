afc423cec676549c7935370b0a20c0e50042c471
visheshdvn <vishesh.dh1@gmail.com>
2021-06-26 00:47:37 +0530

    social share icons created in post page

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -4,6 +4,11 @@ import Link from "next/link"
 import Head from "next/head"
 import { serialize } from "next-mdx-remote/serialize"
 import { MDXRemote } from "next-mdx-remote"
+import {
+  TwitterShareButton,
+  FacebookShareButton,
+  LinkedinShareButton,
+} from "react-share"
 
 import {
   getSlugs,
@@ -21,6 +26,7 @@ import { H1, H2, H3 } from "../../components/blogtext/headings"
 import Img from "../../components/blogtext/image"
 import Ul from "../../components/blogtext/ul"
 import Code from "../../components/blogtext/code"
+import { from } from "@apollo/client"
 
 const Post = ({
   postData: {
@@ -69,48 +75,110 @@ const Post = ({
       </Head>
       <section style={{ maxWidth: "1200px" }} className="mx-auto">
         <div className="container mx-auto horizontal-spacing md:pt-8">
-          <>
+          <div style={{ maxWidth: "730px" }} className="mx-auto">
             <Link href={`/topic/${topic}`}>
               <a
                 style={{ color: `#${associatedColor}` }}
-                className="font-roboto-cond uppercase font-bold text-lg pl-8"
+                className="font-roboto-cond uppercase font-bold text-lg"
               >
                 {topic}
               </a>
             </Link>
 
-            <h1 className="md:text-5xl text-4xl font-roboto-cond font-bold px-8 my-5">
+            <h1 className="md:text-5xl text-4xl font-roboto-cond font-bold my-5 md:leading-tight">
               {title}
             </h1>
 
-            <div className="flex px-8  mb-8">
-              <div
-                style={{
-                  background: "center url('/topicbg/bg.jpg')",
-                  backgroundSize: "cover",
-                }}
-                className="md:h-11 md:w-11 h-10 w-10 rounded-full relative"
-              />
-              <div className="flex flex-col pl-2 justify-around">
-                <h5 className="md:text-base text-sm font-roboto">
-                  Vishesh Dhawan
-                </h5>
-                <div className="flex items-center h-3 text-grayText dark:text-gray-400 overflow-hidden">
-                  <span className="text-sm font-roboto">
-                    <FormattedDate date={date} />
-                  </span>
-                  <span className="mx-2">|</span>
-                  {/* <ClockIcon className="h-full inline mr-1" /> */}
-                  <span className="text-sm font-roboto">
-                    {minutes} min read
-                  </span>
+            <div className="mb-8">
+              <div className="clearfix relative">
+                <div
+                  style={{
+                    background: "center url('/topicbg/bg.jpg')",
+                    backgroundSize: "cover",
+                  }}
+                  className="md:h-11 md:w-11 h-10 w-10 rounded-full relative float-left"
+                />
+                <div className="flex flex-col pl-2 justify-around float-left">
+                  <h5 className="md:text-base text-sm font-roboto">
+                    Vishesh Dhawan
+                  </h5>
+                  <div className="flex items-center h-3 text-grayText dark:text-gray-400 overflow-hidden">
+                    <span className="text-sm font-roboto">
+                      <FormattedDate date={date} />
+                    </span>
+                    <span className="mx-2">|</span>
+                    {/* <ClockIcon className="h-full inline mr-1" /> */}
+                    <span className="text-sm font-roboto">
+                      {minutes} min read
+                    </span>
+                  </div>
+                </div>
+
+                <div className="h-full flex items-center absolute right-0">
+                  <ul>
+                    <li className="ml-3">
+                      <TwitterShareButton
+                        title={title}
+                        url="https://google.com"
+                        className="focus:outline-none border-none"
+                      >
+                        <svg
+                          style={{ color: "#00aced" }}
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 16 16"
+                          className="md:h-6 md:w-6 h-5 w-5"
+                        >
+                          <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+                        </svg>
+                      </TwitterShareButton>
+                    </li>
+                  </ul>
+                  <ul>
+                    <li className="ml-3">
+                      <FacebookShareButton
+                        style={{ color: "#3A5794" }}
+                        quote={title}
+                        url="https://google.com"
+                        className="focus:outline-none border-none"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 16 16"
+                          className="md:h-6 md:w-6 h-5 w-5"
+                        >
+                          <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                        </svg>
+                      </FacebookShareButton>
+                    </li>
+                  </ul>
+                  <ul>
+                    <li className="ml-3">
+                      <LinkedinShareButton
+                        style={{ color: "#0A66C2" }}
+                        title={title}
+                        url="https://google.com"
+                        className="focus:outline-none border-none"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 16 16"
+                          className="md:h-6 md:w-6 h-5 w-5"
+                        >
+                          <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+                        </svg>
+                      </LinkedinShareButton>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
-          </>
+          </div>
 
           <div className="grid gap-4 grid-cols-12 mb-5">
-            <div className="lg:col-span-9 col-span-12">
+            <div className="lg:col-span-12 col-span-12">
               <div className="mb-8">
                 <Image
                   src={`http://${process.env.HOSTNAME}:${process.env.PORT}${banner.url}`}
@@ -121,8 +189,12 @@ const Post = ({
                   className="object-cover object-center"
                 />
               </div>
-              <main key={id} className="lg:px-14 md:px-16 sm:px-8 mt-5 pb-8">
-                <div id="toSpeech" className="font-serif leading-7.5 text-lg">
+              <main key={id} className="lg:px-14 md:px-16 sm:px-8 px-3 mt-5 pb-8">
+                <div
+                  style={{ maxWidth: "730px" }}
+                  id="toSpeech"
+                  className="font-serif leading-7.5 text-lg mx-auto"
+                >
                   <MDXRemote {...content} components={components} />
                 </div>
               </main>
@@ -141,7 +213,7 @@ const Post = ({
               </div> */}
             </div>
 
-            <div className="col-span-3 hidden lg:block">
+            <div className="col-span-3 hidden lg:bloc">
               <div className="border-l pl-3 pb-2">
                 <h1 className="text-4xl font-pt-sans font-bold mb-4">
                   Featured
@@ -200,6 +272,28 @@ const Post = ({
         </div>
       </section> */}
     </>
+  )
+}
+
+function SocialShare({ title }) {
+  return (
+    <li className="ml-3">
+      <TwitterShareButton
+        title={title}
+        url="https://google.com"
+        className="focus:outline-none border-none"
+      >
+        <svg
+          style={{ color: "#00aced" }}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+          className="h-6 w-6"
+        >
+          <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+        </svg>
+      </TwitterShareButton>
+    </li>
   )
 }
 
