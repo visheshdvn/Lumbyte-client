d12de5e4cda177c95dcf4be549ce82f32baf3bcf
visheshdvn <vishesh.dh1@gmail.com>
2021-07-06 12:16:43 +0530

    hidden disclosure button on mobile view

--- components/Navbar.js
+++ components/Navbar.js
@@ -42,7 +42,7 @@ export default function Navbar() {
         <>
           <div className="container horizontal-spacing mx-auto">
             <div className="relative flex items-center justify-between h-auto">
-              <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
+              <div className="absolute inset-y-0 left-0 items-center fle md:hidde hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
