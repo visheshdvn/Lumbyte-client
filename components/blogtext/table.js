5e7cd5dc1ad47fabad21876e3571cccdcd46292d
visheshdvn <vishesh.dh1@gmail.com>
2021-08-12 15:21:37 +0530

    minor changes

--- components/blogtext/table.js
+++ components/blogtext/table.js
@@ -1,8 +1,6 @@
 import React from "react"
 
 const table = ({ children }) => {
-  //   console.log(JSON.stringify(children, null, 4))
-  //   return <div>table here</div>
   return (
     <table id="special-table" className="table-fixed mb-8">
       {children}
