a5d3f454d381f5c5c2695fc242eef1a9c4dba713
Vishesh Dhawan <vishesh.dh1@gmail.com>
2022-02-03 05:12:13 +0530

    publish blogpost api completed

--- components/elements/buttons/buttons.js
+++ components/elements/buttons/buttons.js
@@ -4,7 +4,7 @@ export const PublishButton = ({ text, onClickHandler }) => {
   return (
     <button
       type="button"
-      className="bg-[#1da1f2] border w-28 py-1 font-raleway font-semibold text-base text-white rounded"
+      className="bg-[#1da1f2] w-28 py-1 font-raleway font-semibold text-base text-white rounded"
       onClick={onClickHandler}
     >
       {text}
@@ -12,11 +12,23 @@ export const PublishButton = ({ text, onClickHandler }) => {
   );
 };
 
+export const unPublishButton = ({ text, onClickHandler }) => {
+  return (
+    <button
+      type="button"
+      className="border bg-red-500 w-28 py-1 font-raleway font-semibold text-base text-white rounded"
+      onClick={onClickHandler}
+    >
+      {text}
+    </button>
+  );
+}
+
 export const SaveButton = ({ text, onClickHandler }) => {
   return (
     <button
       type="button"
-      className="bg-[#16A34A] border w-28 py-1 ml-4 font-raleway font-semibold text-base text-white rounded"
+      className="bg-[#16A34A] w-28 py-1 ml-4 font-raleway font-semibold text-base text-white rounded"
       onClick={onClickHandler}
     >
       {text}
--- pages/api/blogposts/publish/[id].js
+++ pages/api/blogposts/publish/[id].js
@@ -1,12 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import nc from "next-connect";
 
-const { blogposts, user } = new PrismaClient();
+const { blogposts } = new PrismaClient();
 
 // middlewares
 
 const handler = nc({
   onError: (err, req, res) => {
+    console.log(err);
     res.status(500).json({ msg: "Server Error" });
   },
   onNoMatch: (req, res, next) => {
@@ -32,9 +33,68 @@ handler.put(async (req, res) => {
 
   const post = await blogposts.findUnique({
     where: { id: +id },
+    select: {
+      id: true,
+      title: true,
+      slug: true,
+      metaDescription: true,
+      excerpt: true,
+      banner: true,
+      banneralt: true,
+      tags: true,
+      authorId: true,
+    },
   });
-  console.log(post);
-  return res.json({ msg: "blogpost published" });
+  // console.log(post);
+
+  const { metaDescription, excerpt, banner, banneralt, tags } = post;
+  // validations
+  let errors = [];
+
+  if (!metaDescription || !metaDescription.trim()) {
+    errors.push({
+      field: "metaDescription",
+      msg: "metaDescription is required",
+    });
+  }
+  if (!excerpt || !excerpt.trim()) {
+    errors.push({
+      field: "excerpt",
+      msg: "excerpt is required",
+    });
+  }
+  if (!banner || !banner.trim()) {
+    errors.push({
+      field: "banner",
+      msg: "banner is required",
+    });
+  }
+  if (!banneralt || !banneralt.trim()) {
+    errors.push({
+      field: "banneralt",
+      msg: "banneralt is required",
+    });
+  }
+
+  if (tags.length === 0) {
+    errors.push({
+      field: "tags",
+      msg: "tags are required",
+    });
+  }
+
+  if (errors.length > 0) {
+    return res.status(400).json({ status: "ERR", errors });
+  }
+
+  await blogposts.update({
+    where: { id: +id },
+    data: {
+      published: true,
+    },
+  });
+
+  return res.json({ msg: "blogpost published", status: "OK" });
 });
 
 export default handler;
