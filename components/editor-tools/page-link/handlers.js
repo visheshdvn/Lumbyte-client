1c67cfe21dd005baeffc8cd097a4a1d574c041bc
Vishesh Dhawan <vishesh.dh1@gmail.com>
2022-04-21 14:50:36 +0530

    api endpoint corrected

--- components/editor-tools/page-link/handlers.js
+++ components/editor-tools/page-link/handlers.js
@@ -2,7 +2,7 @@ import axios from "../../../lib/axios";
 import { isValidURL, ValidateIPaddress } from "../../../utils/checkValidURL";
 
 export async function fetchMetaData(url) {
-  const { data } = await axios.post(`/fetchMetaData/test`, {
+  const { data } = await axios.post(`/fetchMetaData`, {
     url,
   });
   return data;
